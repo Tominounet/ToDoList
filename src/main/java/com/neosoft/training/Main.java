67dcbab1ddbfb821189dd0641af4c4b733ff74b5
t.richard <tom.richard@neo-soft.fr>
2023-11-09 10:01:19 +0100

    Ajout de logback

--- src/main/java/com/neosoft/training/Main.java
+++ src/main/java/com/neosoft/training/Main.java
@@ -1,10 +1,12 @@
 package com.neosoft.training;
-
 import com.neosoft.training.services.DisplayHandler;
 import com.neosoft.training.services.SaisieHandler;
 import com.neosoft.training.utils.Input;
 import com.neosoft.training.model.MenuOption;
 import com.neosoft.training.services.RepositoryHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactoryFriend;
 
 import java.util.Scanner;
 
@@ -12,8 +14,10 @@ public class Main {
     private static DisplayHandler display = DisplayHandler.newInstance();
     private static RepositoryHandler repository = new RepositoryHandler();
     private static SaisieHandler saisie = new SaisieHandler();
+    private final static Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        log.info("Début du programme.");
         Scanner scanner = new Scanner(System.in);
         int inputMenu = -1;
         while (inputMenu != MenuOption.EXIT.getId()) {
@@ -22,9 +26,10 @@ public class Main {
                 inputMenu = Input.inputInt(scanner, inputMenu);
                 MenuOption.find(inputMenu);
             } catch (IllegalArgumentException e) {
-                System.out.println("Erreur : " + e.getMessage());
+                log.error("Erreur : " + e.getMessage());
             }
         }
         System.out.println("Au revoir");
+        log.info("Fin du programme.");
     }
 }
